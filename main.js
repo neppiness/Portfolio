dcfa84989b7d7b04570012fa2c84d2696126d64a
Neppy <neppiness@gmail.com>
2022-03-04 18:44:41 +0900

    navbar JS and centering

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Make navbar opaque when it is not on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+    if(window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+    }
+});
