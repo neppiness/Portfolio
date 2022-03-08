22aeb4bfc903fbc0c0ce3f32da7e6a1ad3f4cd0b
Neppy <neppiness@gmail.com>
2022-03-08 16:31:38 +0900

    Upper Arrow styling and function addition

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ document.addEventListener('scroll', () => {
 });
 
 // Handle scrolling when tapping on the navbar menu
-
 const navbarMenu = document.querySelector('.navbar__menu');
 const homeContactBtn = document.querySelector('button.home__contact');
 
@@ -33,9 +32,7 @@ homeContactBtn.addEventListener('click', (event) => {
     }
 });
 
-// How to remove the repeated part
-
-// Home screen fadeout
+// Home screen fadeout as the window scrolls down
 const homeSection = document.querySelector('section#home');
 const homeTitle = document.querySelector('h1.home__title');
 const homeDescription = document.querySelector('h2.home__description');
@@ -50,4 +47,23 @@ document.addEventListener('scroll', () => {
     homeDescription.style.opacity = homeOpacity;
     homeContact.style.opacity = homeOpacity;
     homeSectionImg.style.opacity = homeOpacity;
+});
+
+// Upper arrow to go top
+const upperArrow = document.querySelector('.fa-angles-up');
+
+upperArrow.addEventListener('click', (event) => {
+    if (event.target.dataset.link != null) {
+        let eventTargetLink = event.target.dataset.link;
+        const targetSection = document.querySelector(eventTargetLink);
+        targetSection.scrollIntoView({behavior: 'smooth'});
+    }
+});
+
+document.addEventListener('scroll', () => {
+    if(window.scrollY > navbarHeight) {
+        upperArrow.classList.add('upperArrow--visible');
+    } else {
+        upperArrow.classList.remove('upperArrow--visible');
+    }
 });
